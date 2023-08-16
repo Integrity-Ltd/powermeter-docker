93b9dbc4b280f218dd9688d96dcea2c7cd83a519
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-08-16 16:19:08 +0200

    LOCALTIMEZONE and docker-sh.bat

--- docker-run-alpine.bat
+++ docker-run-alpine.bat
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 -p 3000:3000 --name powermeter -d -v e:\work\integrity\measurements:/var/integrity/powermeter/measurements integritypz/powermeter:1.0-alpine
+docker run -p 8080:8080 -p 3000:3000 --name powermeter -d -e LOCALTIMEZONE=Europe/Budapest -v e:\work\integrity\measurements:/var/integrity/powermeter/measurements integritypz/powermeter:1.0-alpine
--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$LOCALTIMEZONE" != "" ]; then
+  cp /usr/share/zoneinfo/${LOCALTIMEZONE} /etc/localtime
+  echo $LOCALTIMEZONE > /etc/timezone
+fi
+
 cd ~/powermeter-timed-service
 npx ts-node src/index.ts>log.txt 2>error.log.txt  &
 cd ~/powermeter-api
